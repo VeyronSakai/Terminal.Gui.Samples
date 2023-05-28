2cab5d3549fc820bd481237e751adaa0ac9769b1
VeyronSakai <yuukisakai123@gmail.com>
2023-05-28 23:17:08 +0900

    add menu

--- Terminal.Gui.Samples/Program.cs
+++ Terminal.Gui.Samples/Program.cs
@@ -42,4 +42,29 @@ cli.AddCommand("top", () =>
     Application.Shutdown();
 });
 
+cli.AddCommand("menu", () =>
+{
+    Application.Init();
+    var menu = new MenuBar(new[]
+    {
+        new MenuBarItem("_File", new MenuItem[]
+        {
+            new("_Quit", "", () => { Application.RequestStop(); })
+        }),
+    });
+
+    var win = new Window("Hello")
+    {
+        X = 0,
+        Y = 1,
+        Width = Dim.Fill(),
+        Height = Dim.Fill() - 1
+    };
+
+    // Add both menu and win in a single call
+    Application.Top.Add(menu, win)g;
+    Application.Run();
+    Application.Shutdown();
+});
+
 cli.Run();
