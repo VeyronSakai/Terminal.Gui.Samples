d9274468a59aad6d9bdad2168b97339675224f9e
VeyronSakai <yuukisakai123@gmail.com>
2023-05-28 23:08:36 +0900

    add msg-box command

--- Terminal.Gui.Samples/Program.cs
+++ Terminal.Gui.Samples/Program.cs
@@ -1,11 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Cocona;
+﻿using Cocona;
+using Terminal.Gui;
 
 var cli = CoconaApp.Create();
 
 cli.AddCommand("test",
     () => Console.WriteLine("test")
 ).WithDescription("test command");
+
+cli.AddCommand("msg-box", () =>
+{
+    Application.Init();
+    var n = MessageBox.Query(50, 7,
+        "Question", "Do you like console apps?", "Yes", "No");
+    Application.Shutdown();
+
+    switch (n)
+    {
+        case 0:
+            Console.WriteLine("It's good. Same with me.");
+            break;
+        case 1:
+            Console.WriteLine("That's too bad.");
+            break;
+        default:
+            throw new CommandExitedException(1);
+    }
+});
 
 cli.Run();
